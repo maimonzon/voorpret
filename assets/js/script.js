dd2764ca577ddd6eac529efcd6c1e6b9e8bb7ce6
Mai Monzón <maimonzon@outlook.com>
2021-12-18 22:13:11 -0300

    Agregando cosas al HTML y código JS

---
+++ assets/js/script.js
@@ -0,0 +1,25 @@
+class Viaje {
+    constructor(pais, foto, descripcion) {
+        this.pais = pais;
+        this.foto = foto;
+        this. descripcion = descripcion;
+    }
+}
+
+const destino1 = new Viaje (
+    "Egipto",
+    "https://egyptconsulate.co.uk/privatedemo/wp-content/uploads/2021/01/bigstock-egyptian-pyramids-in-sand-dese-265254931.jpg",
+    "Visita el país de los faraones, envuélvete en su historia y recorre sus principales ciudades: Lúxor, El Cairo, Guiza, Alejandría, entre otras."
+)
+
+const destino2 = new Viaje (
+    "México",
+    "https://cdn.getyourguide.com/img/location/5c9f29710b6f9.jpeg/88.jpg",
+    "Historia, arte, cultura y naturaleza. Visita cada rincón de este país que posee todo para una estadía placentera."
+)
+
+const destino3 = new Viaje (
+    "Grecia",
+    "https://eitrawmaterials.eu/wp-content/uploads/2018/12/dreamstime_l_103810679-1024x647.jpg",
+    "Visita la cuna de la civilización. Una mezcla de historia y paraísos, todo en este destino Europeo."
+)
